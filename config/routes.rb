145aca3442cb588279017deaf665f1af3d8a89c2
Omar Tayfour <omartayfourr@gmail.com>
2021-11-24 15:11:33 +0200

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   # GET About >> AboutController >> views > about > index.html.erb default get "about", to:"about#index"
   get "about", to:"about#index"
+  get "sign_up", to:"registrations#new"
+  
+  post "sign_up", to:"registrations#create"
 
   # localhost:3000 link to become the main. Can be done by get "", to:"main#index" or can be done as seen below
   root to:"main#index"
