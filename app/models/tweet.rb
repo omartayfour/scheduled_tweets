b055ef951a65910f76e66385508d7db087235930
Omar Tayfour <omartayfourr@gmail.com>
2021-11-27 18:55:17 +0200

    part33

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,4 +8,13 @@ class Tweet < ApplicationRecord
   after_initialize do
     self.publish_at ||= 24.hour.from_now
   end
+
+  def published?
+    tweet_id?
+  end
+
+  def publish_to_twitter!
+    tweet = twitter_account.client.update(body)
+    update(tweet_id: tweet.id)
+  end
 end
